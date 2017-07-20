e6b80fc532d3153b1ad309361037995b716bc616
Joseph Prinz <jp102@omics04.openstacklocal>
2017-07-20 16:35:10 -0400

    More or less working, still debugging using -j

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 # Download or update and install various tools
 
 # Global prefix and directories
-PREFIX = $(HOME)/local
+PREFIX = $(PWD)/test
 DOWNLOADS = $(PWD)/downloads
 
-include src/tmux.mk
+#include src/tmux.mk
+include src/nvim.mk
 
 .PHONY: all
-all: tmux-install
+all: nvim-install
 
 .PHONY: clean
 clean:
--- src/nvim.mk
+++ src/nvim.mk
@@ -1,10 +1,11 @@
 # Autotools 
-AUTOCONF_URL = https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz 
-AUTOCONF_DIR = $(DOWNLOADS)/autoconf
+AUTOCONF_URL = https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+AUTOCONF_DIR = autoconf
 AUTOCONF_TARGET = $(AUTOCONF_DIR)/bin/autoconf
 
 # Git repository URL for nvim
-NVIM_GIT = https://github.com/nvim/nvim.git
+NVIM_GIT = https://github.com/neovim/neovim.git
+NVIM_DIR = nvim-git
 NVIM_DEPS = $(AUTOCONF_TARGET)
 NVIM_TARGET = $(PREFIX)/bin/nvim
 
@@ -14,36 +15,43 @@ nvim-install: $(NVIM_TARGET)
 
 # -- autoconf
 $(DOWNLOADS)/autoconf.tar.gz:
-	wget $(AUTOCONF_URL) -O $@
+	mkdir -p $(DOWNLOADS) && wget $(AUTOCONF_URL) -O $@
 
-$(AUTOCONF_DIR)/configure $(DOWNLOADS)/autoconf.tar.gz:
-	mkdir -p $(AUTOCONF_DIR)
-	tar xf $^ --strip-components 1 -c $(AUTOCONF_DIR)
-	touch $@
+$(AUTOCONF_DIR)/configure: $(DOWNLOADS)/autoconf.tar.gz
+	mkdir -p $(AUTOCONF_DIR) && cd $(AUTOCONF_DIR) && tar xf $^ --strip-components 1
 
-$(AUTOCONF_DIR)/Makefile $(AUTOCONF_DIR)/configure:
+$(AUTOCONF_DIR)/Makefile: $(AUTOCONF_DIR)/configure
 	cd $(AUTOCONF_DIR) && ./configure
 
-$(AUTOCONF_TARGET) $(AUTOCONF_DIR)/Makefile:
+$(AUTOCONF_TARGET): $(AUTOCONF_DIR)/Makefile
 	cd $(AUTOCONF_DIR) && $(MAKE)
 
 # -- nvim
-nvim-git/Makefile:
-	git clone $(NVIM_GIT) nvim-git
+$(NVIM_DIR)/Makefile:
+	git clone $(NVIM_GIT) $(NVIM_DIR)
+
+$(NVIM_DIR)/build/bin/nvim: $(NVIM_DIR)/Makefile $(NVIM_DEPS)
+	export PATH="$(AUTOCONF_DIR)/bin:$(PATH)"
+	cd $(NVIM_DIR) && $(MAKE) \
+		MAKE_BUILD_TYPE=Release \
+		CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$(PREFIX)"
+
+$(NVIM_TARGET): $(NVIM_DIR)/build/bin/nvim
+	cd $(NVIM_DIR) && $(MAKE) install
 
 # -- git related phony things
 .PHONY: nvim-clean
-nvim-clean: nvim-git/autogen.sh
-	cd nvim-git && git clean -xfdq && git reset --hard master
+nvim-clean: $(NVIM_DIR)/autogen.sh
+	cd $(NVIM_DIR) && git clean -xfdq && git reset --hard master
 
 .PHONY: nvim-update
-nvim-update: nvim-clean nvim-git/autogen.sh
-	cd nvim-git && git fetch origin
-	cd nvim-git && git reset --hard origin/master
+nvim-update: nvim-clean $(NVIM_DIR)/autogen.sh
+	cd $(NVIM_DIR) && git fetch origin
+	cd $(NVIM_DIR) && git reset --hard origin/master
 	$(MAKE) $(NVIM_TARGET)
 
 .PHONY: nvim-reset
-nvim-reset: nvim-clean nvim-git/autogen.sh
-	cd nvim-git && git reset --hard origin/HEAD~
+nvim-reset: nvim-clean $(NVIM_DIR)/autogen.sh
+	cd $(NVIM_DIR) && git reset --hard origin/HEAD~
 	$(MAKE) $(NVIM_TARGET)
 
