0a14cf6e62b1e169f55ae911fa38166e7e02a5b3
jp102 <jp102@duke.edu>
2017-08-08 11:54:13 -0400

    WIP Adding deps

--- src/ag.mk
+++ src/ag.mk
@@ -1,33 +1,62 @@
+# PCRE Requirements
+PCRE_URL = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.41.tar.gz
+PCRE_DIR = pcre_src
+PCRE_TARGET = $(PREFIX)/lib/libpcre.so
+
+# LZMA
+XZ_URL = https://tukaani.org/xz/xz-5.2.3.tar.gz
+XZ_DIR = lzma_src
+XZ_TARGET = $(PREFIX)/lib/liblzma.so
+
 # Git repository URL for ag
-AG_GIT = https://github.com/neovim/neovim.git
+AG_GIT = https://github.com/ggreer/the_silver_searcher.git
 AG_DIR = ag-git
 AG_TARGET = $(PREFIX)/bin/ag
-
-PCRE_URL = ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.23.tar.gz
-PCRE_DIR = pcre
-PCRE_TARGET = $(PREFIX)/bin/pcre2grep
+AG_DEPS = $(PCRE_TARGET)
 
 # -- Install
 .PHONY: ag-install
 ag-install: $(AG_TARGET)
 
 # -- pcre
-$(PCRE_DIR)/
-# -- pcre
+$(DOWNLOADS)/pcre.tar.gz:
+	mkdir -p $(DOWNLOADS) && wget $(PCRE_URL) -O $@
 
+$(PCRE_DIR)/configure: $(DOWNLOADS)/pcre.tar.gz
+	mkdir -p $(PCRE_DIR) && tar xf $^ --strip-components 1 -C $(PCRE_DIR) && touch $@
+
+$(PCRE_DIR)/Makefile: $(PCRE_DIR)/configure
+	cd $(PCRE_DIR) && ./configure --prefix=$(PREFIX)
+
+$(PCRE_TARGET): $(PCRE_DIR)/Makefile
+	cd $(PCRE_DIR) && $(MAKE) && $(MAKE) install
+
+# -- XZ
+$(DOWNLOADS)/.tar.gz:
+	mkdir -p $(DOWNLOADS) && wget $(XZ_URL) -O $@
+
+$(XZ_DIR)/configure: $(DOWNLOADS)/pcre.tar.gz
+	mkdir -p $(XZ_DIR) && tar xf $^ --strip-components 1 -C $(XZ_DIR) && touch $@
+
+$(XZ_DIR)/Makefile: $(XZ_DIR)/configure
+	cd $(XZ_DIR) && ./configure --prefix=$(PREFIX)
+
+$(PCRE_TARGET): $(XZ_DIR)/Makefile
+	cd $(XZ_DIR) && $(MAKE) && $(MAKE) install
+	
 # -- ag
-$(AG_DIR)/autogen.sh
+$(AG_DIR)/autogen.sh:
 	git clone $(AG_GIT) $(AG_DIR)
 
 $(AG_DIR)/configure: $(AG_DIR)/autogen.sh
 	cd $(AG_DIR) && ./autogen.sh
 
-$(AG_DIR)/Makefile: $(AG_DIR)/configure
-	cd $(AG_DIR) && ./configure
+$(AG_DIR)/Makefile: $(AG_DIR)/configure $(AG_DEPS)
+	export PKG_CONFIG_DIR$(PREFIX)/lib/pkgconfig:$(PKG_CONFIG_DIR) && \
+		cd $(AG_DIR) && ./configure prefix=$(PREFIX)
 
 $(AG_TARGET): $(AG_DIR)/Makefile
 	cd $(AG_DIR) && $(MAKE)
-
 
 # -- git related phony things
 .PHONY: ag-clean
