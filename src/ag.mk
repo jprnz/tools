6a17fad8130621f190fb033ab59a16afe9dc69fd
jp102 <jp102@duke.edu>
2017-08-08 12:59:35 -0400

    Working ag

--- src/ag.mk
+++ src/ag.mk
@@ -5,14 +5,14 @@ PCRE_TARGET = $(PREFIX)/lib/libpcre.so
 
 # LZMA
 XZ_URL = https://tukaani.org/xz/xz-5.2.3.tar.gz
-XZ_DIR = lzma_src
+XZ_DIR = xz_src
 XZ_TARGET = $(PREFIX)/lib/liblzma.so
 
 # Git repository URL for ag
 AG_GIT = https://github.com/ggreer/the_silver_searcher.git
 AG_DIR = ag-git
 AG_TARGET = $(PREFIX)/bin/ag
-AG_DEPS = $(PCRE_TARGET)
+AG_DEPS = $(PCRE_TARGET) $(XZ_TARGET)
 
 # -- Install
 .PHONY: ag-install
@@ -26,23 +26,23 @@ $(PCRE_DIR)/configure: $(DOWNLOADS)/pcre.tar.gz
 	mkdir -p $(PCRE_DIR) && tar xf $^ --strip-components 1 -C $(PCRE_DIR) && touch $@
 
 $(PCRE_DIR)/Makefile: $(PCRE_DIR)/configure
-	cd $(PCRE_DIR) && ./configure --prefix=$(PREFIX)
+	cd $(PCRE_DIR) && ./configure --prefix=$(PREFIX) && touch Makefile
 
 $(PCRE_TARGET): $(PCRE_DIR)/Makefile
-	cd $(PCRE_DIR) && $(MAKE) && $(MAKE) install
+	cd $(PCRE_DIR) && $(MAKE) && $(MAKE) install && touch $@
 
 # -- XZ
-$(DOWNLOADS)/.tar.gz:
+$(DOWNLOADS)/xz.tar.gz:
 	mkdir -p $(DOWNLOADS) && wget $(XZ_URL) -O $@
 
-$(XZ_DIR)/configure: $(DOWNLOADS)/pcre.tar.gz
+$(XZ_DIR)/configure: $(DOWNLOADS)/xz.tar.gz
 	mkdir -p $(XZ_DIR) && tar xf $^ --strip-components 1 -C $(XZ_DIR) && touch $@
 
 $(XZ_DIR)/Makefile: $(XZ_DIR)/configure
-	cd $(XZ_DIR) && ./configure --prefix=$(PREFIX)
+	cd $(XZ_DIR) && ./configure --prefix=$(PREFIX) && touch Makefile
 
-$(PCRE_TARGET): $(XZ_DIR)/Makefile
-	cd $(XZ_DIR) && $(MAKE) && $(MAKE) install
+$(XZ_TARGET): $(XZ_DIR)/Makefile
+	cd $(XZ_DIR) && $(MAKE) && $(MAKE) install && touch $@
 	
 # -- ag
 $(AG_DIR)/autogen.sh:
@@ -52,11 +52,13 @@ $(AG_DIR)/configure: $(AG_DIR)/autogen.sh
 	cd $(AG_DIR) && ./autogen.sh
 
 $(AG_DIR)/Makefile: $(AG_DIR)/configure $(AG_DEPS)
-	export PKG_CONFIG_DIR$(PREFIX)/lib/pkgconfig:$(PKG_CONFIG_DIR) && \
-		cd $(AG_DIR) && ./configure prefix=$(PREFIX)
+	export PKG_CONFIG_PATH=$(PREFIX)/lib/pkgconfig && \
+	export LD_LIBRARY_PATH=$(PREFIX)/lib && \
+	export CFLAGS="-I$(PREFIX)/include -I$(PREFIX)/lzma" && \
+		cd $(AG_DIR) && ./configure prefix=$(PREFIX) && touch Makefile
 
 $(AG_TARGET): $(AG_DIR)/Makefile
-	cd $(AG_DIR) && $(MAKE)
+	cd $(AG_DIR) && $(MAKE) && $(MAKE) install && touch $@
 
 # -- git related phony things
 .PHONY: ag-clean
